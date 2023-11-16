7b6f5883c91810495eb1b83361c7e0a0c1e3c8a9
jeonbyungho <byho.jeon@gmail.com>
2023-11-16 17:20:31 +0900

    회원 가입 구현

--- workspace/prj/coverletter_site/forms.py
+++ workspace/prj/coverletter_site/forms.py
@@ -1,12 +1,12 @@
 from .models import *
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm 
+from django.contrib.auth.forms import UserCreationForm 
 
-class UserForm(UserChangeForm):
+class UserForm(UserCreationForm):
 
    email = forms.EmailField()
 
-   class Mata:
+   class Meta:
       model = User
-      fields = ["username", "email"]
+      fields = ("username", "email")
--- workspace/prj/coverletter_site/views.py
+++ workspace/prj/coverletter_site/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 
 from .models import *
 from .forms import *
 
+# 메인
 def index(request):
    return render(request, "coverletter_site/index.html")
 
+# 회원가입
 def join(request):
-   form = UserForm
-   return render(request, "coverletter_site/join.html", {'form':form})
+   # POST
+   if request.method == "POST":
+      form = UserForm(request.POST)
+      if form.is_valid():
+         form.save()
+         print(form)
+         return redirect('login')
+   # GET
+   form = UserForm()
+   return render(request, "coverletter_site/join.html", {'userForm':form})
 
+# 문서 업로드
 def coverletter_upload(request):
    return render(request, "coverletter_site/coverletter_upload.html")
 
