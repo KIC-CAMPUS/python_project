4d28a5549a7090293227865f13d73e5aaf00ed41
jeonbyungho <byho.jeon@gmail.com>
2023-11-21 17:16:26 +0900

    이용 후기와 댓글 모델 작성

--- workspace/prj/review_site/models.py
+++ workspace/prj/review_site/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from member_site.models import User
 
-# Create your models here.
+# 이용 후기
+class Review(models.Model):
+   author = models.ForeignKey(User, on_delete=models.CASCADE)
+   title = models.CharField(max_length=100)
+   content = models.TextField()
+
+   create_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+# 이용 후기 댓글
+class Reply(models.Model):
+   author = models.ForeignKey(User, on_delete=models.CASCADE)
+   review = models.ForeignKey(Review, on_delete=models.CASCADE)
+   content = models.TextField()
+
+   create_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
