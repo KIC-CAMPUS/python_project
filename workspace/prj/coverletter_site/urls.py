fa25947d112c555e4bab635a0c779b537ddb47f3
dlwlgjsdlek <wonjhl@naver.com>
2023-11-24 14:41:24 +0900

    정렬 기능 추가

--- workspace/prj/coverletter_site/urls.py
+++ workspace/prj/coverletter_site/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
    path("upload/", views.CoverLetterCreated.as_view(), name='upload'),
    path("list/", views.CoverLetterList.as_view(), name='result_list'),
    path("<int:pk>/", views.CoverLetterDetail.as_view(), name='coverletter_detail'),
-   path("delete/", views.CoverLetterDelete, name='cover_letter_delete'),
-   path('list/search/<str:q>/', views.PostSearch.as_view())
+   path("delete/", views.coverLetterDelete, name='cover_letter_delete'),
+   path('list/search/<str:q>/', views.PostSearch.as_view()),
+   path('list/sort/<q>/', views.CoverLetterSortList.as_view()),
+   path("list/", views.CoverLetterSortList.as_view(), name=''),
    # path(r'^like/$', views.post_like, name='post_like'),
 ]
--- workspace/prj/member_site/views.py
+++ workspace/prj/member_site/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
+from django.urls import reverse_lazy
+
 from .forms import UserForm
+from coverletter_site.views import CoverLetterList, coverLetterDelete
+
+from coverletter_site.models import CoverLetter
+
 
 # 회원가입
 def join(request):
@@ -17,10 +25,47 @@ def join(request):
    return render(request, "member/join.html", {'form': form})
 
 # 마이페이지
-def mypage(request):
-   return render(request, "member/mypage.html")
+class MypageView(CoverLetterList):
+   template_name = "member/mypage.html"
 
 # 페이지 볼려고 추가했습니다. 무시하셔도 될거 같아요
 # 마이페이지 수정
 def mypage_edit(request):
    return render(request, "member/mypage_edit.html")
+
+def mypage_coverLetterDelete(request):
+   coverLetterDelete(request)
+   return redirect(reverse_lazy('mypage'))
+
+def post_like(request):
+   if request.method == 'POST':
+      pk = request.POST.get('pk', None)  # ajax 통신을 통해서 template에서 POST방식으로 전달
+      post = get_object_or_404(CoverLetter, pk=pk)
+      post_like, post_like_created = post.like_set.get_or_create(user=request.user)
+
+      if not post_like_created:
+         post_like.delete()
+
+      return HttpResponse(content_type="application/json")
+
+class PostSearch(MypageView):
+   def get_queryset(self):
+      q = self.kwargs['q']
+      post_list = super().get_queryset().filter(
+         Q(title__contains=q)
+      ).distinct()
+      return post_list
+
+class Mypage_CoverLetterSortList(MypageView):
+   def get_queryset(self):
+      q = self.kwargs['q']
+      if q == "all":
+         self.ordering = ['-pk']
+      elif q == "latest":
+         self.ordering = ['-create_at']
+      elif q == "high":
+         self.ordering = ['-rate']
+      elif q == "low":
+         self.ordering = ['rate']
+
+      return super().get_queryset()
