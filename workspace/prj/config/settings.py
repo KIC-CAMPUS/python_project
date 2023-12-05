4fa0585a93011ef2ed0761390e6063244d95016d
jeonbyungho <byho.jeon@gmail.com>
2023-12-05 10:29:30 +0900

    발표 준비를 위한 ALLOWED_HOSTS 값 수정

--- workspace/prj/config/settings.py
+++ workspace/prj/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eml6f(0lrn$ex*&tp3cosdvwk62h9972#(7p9cn)x#a6fgg6!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
